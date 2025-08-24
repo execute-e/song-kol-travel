d9eb4ad7f79d7018a4c0e2883240dd4113b61e6f
executed <execute.eee@yandex.ru>
2025-08-24 17:25:23 +0300

    build the app

--- src/scripts/main.js
+++
@@ -1,7 +0,0 @@
-import Header from "./Header";
-import Slider from "./Slider";
-import Form from "./Form";
-
-new Header();
-new Slider(document.querySelector("[data-js-hero-slider]"));
-new Form();
